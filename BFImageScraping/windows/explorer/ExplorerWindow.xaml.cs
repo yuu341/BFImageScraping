2b957321eaa1b0992c2592b27ace8f4cd43b3bf3
yusuke furuya <oz.g.341@gmail.com>
2014-06-06 22:22:14 +0900

    EntityFramework 及び HtmlAgilityPackの追加

---
+++ BFImageScraping/windows/explorer/ExplorerWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BFImageScraping.windows.explorer
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class ExplorerWindow : Window
+    {
+        public ExplorerWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
